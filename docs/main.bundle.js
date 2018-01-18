07ab0ab9cb9f32f930163da2fe49bc29fa7e7946
Julian Cable <cablej01@mc-s104758.local>
2018-01-18 22:08:07 +0000

    rename

--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -57,7 +57,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 
 var AppComponent = (function () {
     function AppComponent() {
-        this.title = 'Vocab Predictor';
+        this.title = 'BBC New Speak';
     }
     AppComponent = __decorate([
         Object(__WEBPACK_IMPORTED_MODULE_0__angular_core__["m" /* Component */])({
